9e7d5c37449495af674c1a9fda7754c2d0847342
Mega <yossef.ayman1366@gmail.com>
2025-05-07 12:30:42 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,10 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         String[] service_start = { "cmd.exe", "/c", "sc", "start", "SERVICE_NAME" };
 
@@ -10,10 +13,28 @@ public class App {
         String[] service_check = { "cmd.exe", "/c", "sc", "query", "SERVICE_NAME", "|", "find", "/C",
                 "\"RUNNING\"" };
 
-        try {
-            Process process = Runtime.getRuntime().exec(service_start);
-        } catch (IOException e) {
-            e.printStackTrace();
+        int user_choose = scanner.nextInt();
+
+        if (user_choose == 1) {
+            try {
+                Runtime.getRuntime().exec(service_start);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (user_choose == 2) {
+            try {
+                Runtime.getRuntime().exec(service_stop);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (user_choose == 3) {
+            try {
+                Runtime.getRuntime().exec(service_check);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("choose");
         }
 
     }
