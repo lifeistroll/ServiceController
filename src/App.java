d164f6091fe0324182c9b25a5cf61c4af0e8af21
Mega <yossef.ayman1366@gmail.com>
2025-05-07 12:25:26 +0300

    commit

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
 
+        String[] service_start = { "cmd.exe", "/c", "sc", "start", "SERVICE_NAME" };
+
+        String[] service_stop = { "cmd.exe", "/c", "sc", "stop", "SERVICE_NAME" };
+
+        String[] service_check = { "cmd.exe", "/c", "sc", "query", "SERVICE_NAME", "|", "find", "/C",
+                "\"RUNNING\"" };
+
+        try {
+            Process process = Runtime.getRuntime().exec(service_start);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public static startSer(){
-        
-    }
 }
